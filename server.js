b2307eb4af8578d5606a47820cb111150579c7c3
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-11-23 22:47:50 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set("port", (process.env.PORT || 5000));
 
+app.get("/getPrintJobs", getPrintJobs);
+ 
 app.listen(app.get("port"), function () {
     console.log("Now Listening for connections on port: ", app.get("port"));
 });
+
+function getPrintJobs(request, response) {
+	console.log("Getting print jobs");
+};
